8661e7ce40dccd1ceb967bd275348e56c8266961
Ahmed.Shaaban.Elbehary <as.elbehary@gmail.com>
2020-08-14 17:33:43 +0200

    1

--- src/app/template-variabl/template-variabl.component.ts
+++ src/app/template-variabl/template-variabl.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * *template variables used insted of $event we can give the element variables and using it.
+ */
 @Component({
   selector: 'app-template-variabl',
   template: `
     <p>
       template-variabl works!
     </p>
-    <input (keyup.enter)="onkeyUp($event)">
+    <!-- <input (keyup.enter)="onkeyUp($event)"> -->
+    <input #email (keyup.enter) = "onKeyUp(email.value)">
   `,
   styles: [
   ]
 })
 export class TemplateVariablComponent implements OnInit {
-
-  onkeyUp($event): void{
-    alert($event.target.value);
+  onKeyUp(email): void{
+    console.log(email);
   }
+  // onkeyUp($event): void{
+  //   alert($event.target.value);
+  // }
   constructor() { }
 
   ngOnInit(): void {
